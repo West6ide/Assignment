2e60cd27999bafb5a8058ab9e28117acdb2db33e
Sultan <130346122+West6ide@users.noreply.github.com>
2024-04-26 14:58:12 +0500

    Update assignment3.go

--- assignment3.go
+++ assignment3.go
@@ -37,14 +37,12 @@ const (
 func init() {
 	ctx = context.Background()
 
-	// Подключение к Redis
 	redisClient = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		Password: "", // нет установленного пароля
-		DB:       0,  // использовать базу данных по умолчанию
+		Password: "", 
+		DB:       0, 
 	})
 
-	// Подключение к PostgreSQL
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	var err error
 	db, err = sqlx.Connect("postgres", psqlconn)
@@ -62,17 +60,14 @@ func getProductByIDHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Попытка получить данные о продукте из кэша Redis
 	cachedProduct, err := redisClient.Get(ctx, "product:"+productIDStr).Result()
 	if err == nil {
-		// Возвращаем данные о продукте из кэша Redis
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(cachedProduct))
 		return
 	}
 
-	// Если данные о продукте не найдены в кэше Redis, получаем их из базы данных PostgreSQL
 	var product Product
 	err = db.Get(&product, "SELECT * FROM products WHERE id = $1", productID)
 	if err != nil {
@@ -80,13 +75,10 @@ func getProductByIDHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Преобразование продукта в JSON
 	productJSON, _ := json.Marshal(product)
 
-	// Сохранение данных о продукте в кэше Redis с TTL 24 часа
 	redisClient.Set(ctx, "product:"+productIDStr, string(productJSON), 24*time.Hour)
 
-	// Возвращаем данные о продукте клиенту
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(productJSON)
